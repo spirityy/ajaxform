8185c02befc8f8f63803aee65c578787da73bea0
sunpeng <spirityy109@gmail.com>
2015-07-20 12:31:49 +0800

    init submit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # ajaxform
-simple ajax submit form plugin
+a simple ajax submit plugin
+
+
+####ajaxSubmit( options,callback )
+
+Submit the form via Jquery AJAX.
+
+Url,Method,Data,etc.. params from the form.
+
+````javascript
+// bind submit to form
+$('form').on('submit', function(e) {
+	e.preventDefault(); // prevent native submit
+	$(this).ajaxSubmit({
+    extraData:{apple:1000}, //Additional parameters '&apple=1000'
+		success:function(){
+			//do something after form submit via ajax
+		}
+	});
+});
+````
+
+####options( extend jquery ajax options and )
+
+extraData(object) : Extra data，simulation of hidden input value.
---
+++ ajaxform.js
@@ -0,0 +1,36 @@
+/*!
+ *  form ajax submit
+ */
+
+(function($) {
+
+  "use strict";
+
+  $.fn.ajaxSubmit = function(options,callback) {
+
+    var opts = {};
+
+    if (typeof options == 'function') {
+        opts.success=options;
+    }
+
+    var initopts = {
+      url:$(this).attr('action'),
+      method:$(this).attr('method')||'post',
+      data:$(this).serialize()||'',
+      success:callback
+    };
+
+    if(options.extraData){
+      if(typeof options.extraData !== 'object') return;
+      initopts.data+='&'+$.param(options.extraData);
+    }
+
+    opts = $.extend(initopts,options);
+
+    $.ajax(opts);
+
+  };
+
+})(jQuery);
+
